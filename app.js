ec35ec1f3d2def8d2443dfa0ac8e79a1ee085a38
HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2025-11-15 05:24:36 -0800

    init my actual project

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+let num = 44.6;
+let num2 = 79;
+// let a = Math.round(num);  This One Is Round The  Number Nearest Integer
+// let b = Math.round(num2); 
+// let a = Math.pow(6,2); This One is Power Of Number 2
+// let a = Math.sqrt(32); This One is Return Squre Root of Number
+// let a = Math.ceil(5.2);  This One Greater Then And Equal To 5
+// let a = Math.floor(5.8); This One Is Less Then to 5
+// let a = Math.min(5,3,7); This One Is Minimum value nearest value
+// let a  = Math.max(4,10,15); This One Is Maxium value Of highest Value
+console.log(a)
